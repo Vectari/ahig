65228f562bf7735490527df73fc4590d7c57009a
Vectari <mateusz.majer996@gmail.com>
2025-05-29 01:11:17 +0200

    feat: install basic dep, create basic folders and files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,68 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { createGlobalStyle } from "styled-components";
+// import { theme } from "./theme";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { NavBar } from "./components/NavBar/NavBar";
+import { Footer } from "./components/Footer/Footer";
+import { Home } from "./view/Home/Home";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const GlobalStyles = createGlobalStyle`
+body {
+  margin: 0 auto;
+  background-color: #979797;
+}`;
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        {/* <AlertComponent /> */}
+        <NavBar />
+        <Outlet />
+        <Footer />
+      </>
+    ),
+    children: [
+      {
+        index: true,
+        element: (
+          <>
+            <Home />
+          </>
+        ),
+      },
+      // {
+      //   path: "/info",
+      //   element: <Info />,
+      // },
+      // {
+      //   path: "/pricelist",
+      //   element: <PriceList />,
+      // },
+      // {
+      //   path: "/reviews",
+      //   element: <Reviews />,
+      // },
+      // {
+      //   path: "/contact",
+      //   element: <Contact />,
+      // },
+      // {
+      //   path: "/map",
+      //   element: <Map />,
+      // },
+      // {
+      //   path: "/alert",
+      //   element: <Alert />,
+      // },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <GlobalStyles />
+    <RouterProvider router={router} />
+  </>
+);
