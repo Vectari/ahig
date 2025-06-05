d6b7bd7ddf3f68e013e0ce59178bbd8518ef17a0
Vectari <mateusz.majer996@gmail.com>
2025-06-05 21:05:43 +0200

    feat: update dictionary and navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,53 @@
+import styled from "styled-components";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { useTranslation } from "../../hooks/useTranslation";
+import { LanguageSelect } from "../LanguageSelect/LanguageSelect";
+
+const NavBarWrapper = styled.nav``;
+
+const StyledLogoContainer = styled.div``;
+
+const NavLinkWrapper = styled.div``;
+
+const StyledNavLink = styled(NavLink)`
+  &:hover {
+    transition: 0.3s;
+  }
+
+  &.active {
+    pointer-events: none;
+  }
+`;
+
+const StyledFontIcon = styled.span``;
+
 export function NavBar() {
+  const [active, setActive] = useState(false);
+  const { translate } = useTranslation();
+
   return (
-    <>
-      <h1>NavBar</h1>
-    </>
+    <NavBarWrapper>
+      <StyledLogoContainer>{/* <Logo to={"/"} /> */}</StyledLogoContainer>
+      <StyledFontIcon onClick={() => setActive(!active)}>
+        &equiv;
+      </StyledFontIcon>
+      <NavLinkWrapper active={active} onClick={() => setActive(!active)}>
+        <StyledNavLink to="/">{translate("NavBar", "home")}</StyledNavLink>
+        <StyledNavLink to="/team">{translate("NavBar", "team")}</StyledNavLink>
+        <StyledNavLink to="/pricelist">
+          {translate("NavBar", "price")}
+        </StyledNavLink>
+        <StyledNavLink to="/gallery">
+          {translate("NavBar", "gallery")}
+        </StyledNavLink>
+        <StyledNavLink to="/contact">
+          {translate("NavBar", "contact")}
+        </StyledNavLink>
+        <LanguageSelect display={false} />
+      </NavLinkWrapper>
+
+      {/* <NavLink to="/alert">Alert</NavLink> */}
+    </NavBarWrapper>
   );
 }
--- src/dictionary.js
+++ src/dictionary.js
@@ -1,1 +1,26 @@
-export const dictionary = {};
+export const dictionary = {
+  NavBar: {
+    home: {
+      pl: "Strona Główna",
+      en: "Home Page",
+    },
+    team: {
+      pl: "Nas Zespół",
+      en: "Our Team",
+    },
+    price: {
+      pl: "Cennik",
+      en: "Price List",
+    },
+    gallery: {
+      pl: "Galeria",
+      en: "Gallery",
+    },
+    contact: {
+      pl: "Kontakt",
+      en: "Contact",
+    },
+  },
+};
+
+export default dictionary;
