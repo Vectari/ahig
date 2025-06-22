f0715ea15b009599c853a2b347269e550ef19fdb
Vectari <mateusz.majer996@gmail.com>
2025-06-22 02:42:48 +0200

    feat: add custom font

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { createGlobalStyle } from "styled-components";
-// import { theme } from "./theme";
+import { theme } from "./theme";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import { LanguageProvider } from "./contexts/LanguageProvider";
 import { NavBar } from "./components/NavBar/NavBar";
@@ -14,10 +14,16 @@ import { Blog } from "./view/Blog/Blog";
 import { Alert } from "./view/Alert/Alert";
 
 const GlobalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+
 body {
   margin: 0 auto;
-  background-color: #979797;
-}`;
+  /* max-width: 1400px; */
+  background-color: ${theme.background};
+  color: ${theme.fonts};
+  font-family: "Roboto", sans-serif;
+}
+`;
 
 const router = createBrowserRouter([
   {
